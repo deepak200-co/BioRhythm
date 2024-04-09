aa216b2f715713b25248bc20ab3502d35fa5af22
deepak200-co <deepak9063154@gmail.com>
2024-04-09 13:34:08 +0530

    changes done

--- frontend/src/Biorhythm/BiorhythmChart.js
+++ frontend/src/Biorhythm/BiorhythmChart.js
@@ -18,11 +18,11 @@ const BiorhythmChart = ({ birthDate, presentDayPhysical, presentDayEmotional, pr
       const startDate = currentDate.clone().subtract(0, 'days');
       const chartData = [];
 
-      for (let i = 0; i < 21; i++) {
+      for (let i = 0; i < 11; i++) {
         const currentDate = startDate.clone().add(i, 'days');
         const daysSinceBirth = Math.floor((currentDate - moment(birthDate)) / (1000 * 60 * 60 * 24));
-        // const physical = Math.round(Math.sin((2 * Math.PI * daysSinceBirth) / 23) * 100);
-        // const emotional = Math.round(Math.sin((2 * Math.PI * daysSinceBirth) / 28) * 100);
+        // const physical = Math.round(Math.sin((2 * Math.PI * daysSinceBirth) / 23) * 100);   
+        // const emotional = Math.round(Math.sin((2 * Math.PI * daysSinceBirth) / 28) * 100);  
         // const intellectual = Math.round(Math.sin((2 * Math.PI * daysSinceBirth) / 33) * 100);
         const physical = Math.round((Math.sin((2 * Math.PI * daysSinceBirth) / 23) + 1) * 50);
         const emotional = Math.round((Math.sin((2 * Math.PI * daysSinceBirth) / 28) + 1) * 50);
@@ -31,7 +31,7 @@ const BiorhythmChart = ({ birthDate, presentDayPhysical, presentDayEmotional, pr
         const average = Math.round((physical + emotional + intellectual) / 3);
 
         chartData.push({
-          day: currentDate.format('DD/MM/YYYY'),
+          day: currentDate.format('DD/MM'),
           physical,
           emotional,
           intellectual,
@@ -61,10 +61,10 @@ const BiorhythmChart = ({ birthDate, presentDayPhysical, presentDayEmotional, pr
       <ResponsiveContainer width="100%" height={600}>
         <LineChart data={chartData}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="day" label={{ value: '', position: 'insideBottom' }} />
+          <XAxis dataKey="day" label={{ value: '', position: 'insideBottom' }} interval={0} />
           <YAxis type="number" domain={[0, 100]} label={{ value: 'Biorhythm Value', angle: -90, position: 'insideLeft' }} />
           <Tooltip
-            formatter={(value) => (value < 0 ? (-value).toFixed(2) : value.toFixed(2))}
+            formatter={(value) => (value < 0 ? (-value).toFixed(3) : value.toFixed(3))}
           />
           <Legend verticalAlign="top" align="right" />
           <Line type="monotone" dataKey="physical" stroke="#ff6347" strokeWidth={2} dot={{ r: 4 }} />
--- frontend/src/Main/Main.js
+++ frontend/src/Main/Main.js
@@ -16,6 +16,114 @@ function Main({ user }) {
   const [presentDayIntellectual, setPresentDayIntellectual] = useState(null);
   const [presentDayAverage, setPresentDayAverage] = useState(null);
   const [userData, setUserData] = useState({})
+  const getTextForPhysical = (value) => {
+    if (value >= 0 && value <= 10) {
+      return "Your physical energy is at a low point. It’s a good time for rest and relaxation. Try to avoid strenuous activities.";
+    } else if (value >= 11 && value <= 20) {
+      return " You’re starting to feel more energetic. Light exercise like stretching or a short walk would be beneficial.";
+    } else if (value >= 21 && value <= 30) {
+      return " Your energy levels are increasing. You can handle moderate physical activities like a brisk walk or light workout.";
+    }else if (value >= 31 && value <= 40) {
+      return "You’re feeling strong and capable. It’s a good time for more strenuous physical activities like a run or a gym workout.";
+    }else if (value >= 41 && value <= 50) {
+      return "You’re at the peak of your physical cycle. You can handle high-intensity workouts. This is a great time to push your limits.";
+    }else if (value >= 51 && value <= 60) {
+      return "Your physical energy is still high, but starting to decline. Continue with your regular activities, but start to wind down.";
+    }else if (value >= 61 && value <= 70) {
+      return " Your energy levels are decreasing. It’s a good time to start focusing on recovery activities like yoga or meditation.";
+    }else if (value >= 71 && value <= 80) {
+      return " You’re entering a period of lower physical energy. Focus on rest and recuperation. Light activities like walking or stretching can help.";
+    }else if (value >= 81 && value <= 90) {
+      return "Your physical energy is low. It’s a good time for gentle activities and relaxation. Consider activities like reading or listening to music.";
+    }else if (value >= 91 && value <= 100) {
+      return "You’re at the lowest point of your physical cycle. Your body needs time to rejuvenate. Prioritize restful activities like sleep and gentle yoga.";
+    }
+    else {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    }
+  };
+
+  const getTextForEmotional = (value) => {
+    if (value >= 0 && value <= 10) {
+      return "Your emotional energy is at a low point. It’s a good time for relaxation and self-care. Try to avoid stressful situations.";
+    } else if (value >= 11 && value <= 20) {
+      return "You’re starting to feel more emotionally balanced. Light social activities like chatting with a friend or spending time with family would be beneficial.";
+    } else if (value >= 21 && value <= 30) {
+      return " Your emotional stability is increasing. You can handle moderate social interactions like attending a social gathering or meeting new people.";
+    }else if (value >= 31 && value <= 40) {
+      return "You’re feeling emotionally strong and capable. It’s a good time for more challenging social activities like public speaking or resolving conflicts.";
+    }else if (value >= 41 && value <= 50) {
+      return " You’re at the peak of your emotional cycle. You can handle high-intensity social interactions. This is a great time to engage in activities that require emotional strength.";
+    }else if (value >= 51 && value <= 60) {
+      return "Your emotional energy is still high, but starting to decline. Continue with your regular activities, but start to wind down.";
+    }else if (value >= 61 && value <= 70) {
+      return "Your emotional stability is decreasing. It’s a good time to start focusing on self-care. Light activities like reading a book or taking a bath can help.";
+    }else if (value >= 71 && value <= 80) {
+      return " You’re entering a period of lower emotional energy. Focus on rest and self-care. Consider activities like meditation or spending time with loved ones.";
+    }else if (value >= 81 && value <= 90) {
+      return " Your emotional energy is low. It’s a good time for gentle activities and relaxation. Consider activities like watching a movie or enjoying a hobby.";
+    }else if (value >= 91 && value <= 100) {
+      return "You’re at the lowest point of your emotional cycle. Your emotional resilience is low. Engage in self-care activities that promote emotional well-being, like meditation or spending time with loved ones.";
+    }
+    else {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    }
+  };
+
+  const getTextForIntellectual = (value) => {
+    if (value >= 0 && value <= 10) {
+      return "Your intellectual energy is at a low point. It’s a good time for relaxation and non-demanding tasks. Try to avoid complex problem-solving tasks.";
+    } else if (value >= 11 && value <= 20) {
+      return " You’re starting to feel more mentally alert. Light cognitive activities like reading a newspaper or solving a crossword puzzle would be beneficial.";
+    } else if (value >= 21 && value <= 30) {
+      return "Your intellectual capabilities are increasing. You can handle moderate cognitive tasks like studying a new topic or learning a new skill.";
+    }else if (value >= 31 && value <= 40) {
+      return " You’re feeling mentally strong and capable. It’s a good time for more challenging cognitive activities like solving complex problems or brainstorming ideas.";
+    }else if (value >= 41 && value <= 50) {
+      return "You’re at the peak of your intellectual cycle. You can handle high-intensity cognitive tasks. This is a great time to tackle challenging intellectual work.";
+    }else if (value >= 51 && value <= 60) {
+      return "Your intellectual energy is still high, but starting to decline. Continue with your regular activities, but start to wind down.";
+    }else if (value >= 61 && value <= 70) {
+      return " Your intellectual capabilities are decreasing. It’s a good time to start focusing on relaxation. Light activities like reading a novel or watching a movie can help.";
+    }else if (value >= 71 && value <= 80) {
+      return "You’re entering a period of lower intellectual energy. Focus on rest and recuperation. Consider activities like meditation or listening to music.";
+    }else if (value >= 81 && value <= 90) {
+      return "Your intellectual energy is low. It’s a good time for gentle activities and relaxation. Consider activities like taking a walk or enjoying nature.";
+    }else if (value >= 91 && value <= 100) {
+      return "You’re at the lowest point of your intellectual cycle. Your cognitive resources are depleted. Engage in relaxing activities that don’t require much mental effort, like watching a movie or listening to music.";
+    }
+    else {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    }
+  };
+
+  const getTextForAverage = (value) => {
+    if (value >= 0 && value <= 10) {
+      return "Your overall energy is at a low point. It’s a good time for rest and relaxation. Try to avoid demanding tasks.";
+    } else if (value >= 11 && value <= 20) {
+      return "You’re starting to feel more balanced. Light activities like taking a walk or doing some light reading would be beneficial.";
+    } else if (value >= 21 && value <= 30) {
+      return " Your overall capabilities are increasing. You can handle moderate tasks like doing some housework or studying a new topic.";
+    }else if (value >= 31 && value <= 40) {
+      return "You’re feeling strong and capable. It’s a good time for more challenging activities like tackling a complex project or learning a new skill.";
+    }else if (value >= 41 && value <= 50) {
+      return " You’re at the peak of your average cycle. You can handle high-intensity tasks. This is a great time to push your limits.";
+    }else if (value >= 51 && value <= 60) {
+      return "Your overall energy is still high, but starting to decline. Continue with your regular activities, but start to wind down";
+    }else if (value >= 61 && value <= 70) {
+      return "Your overall capabilities are decreasing. It’s a good time to start focusing on relaxation. Light activities like reading a book or watching a movie can help";
+    }else if (value >= 71 && value <= 80) {
+      return "You’re entering a period of lower overall energy. Focus on rest and recuperation. Consider activities like taking a bath or listening to music.";
+    }else if (value >= 81 && value <= 90) {
+      return " Your overall energy is low. It’s a good time for gentle activities and relaxation. Consider activities like taking a walk or enjoying nature.";
+    }else if (value >= 91 && value <= 100) {
+      return "You’re at the lowest point of your average cycle. Overall, your energy levels are low. It’s a good time to engage in calming activities like reading a book or taking a leisurely walk.";
+    }
+    else {
+      return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    }
+  };
+
 
   useEffect(() => {
     AOS.init({ duration: 1000 });
@@ -83,10 +191,10 @@ function Main({ user }) {
               />
               <h1 className='inter' data-aos='fade-up' data-aos-duration='1000'> Daily Interpretation </h1>
               <div className="text-boxes">
-                <div className="text-box" data-aos='fade-right' data-aos-duration='1000' ><h2>Physical</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> </div>
-                <div className="text-box" data-aos='fade-up-left' data-aos-duration='1000'><h2>Intellectual</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> </div>
-                <div className="text-box" data-aos='fade-up-right' data-aos-duration='1000'><h2>Emotional</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> </div>
-                <div className="text-box" data-aos='fade-up' data-aos-duration='1000'><h2>Average</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> </div>
+                <div className="text-box" data-aos='fade-right' data-aos-duration='1000' ><h2>Physical</h2>  <p>{getTextForPhysical(presentDayPhysical)}</p></div>
+                <div className="text-box" data-aos='fade-up-left' data-aos-duration='1000'><h2>Intellectual</h2><p>{getTextForIntellectual(presentDayIntellectual)}</p></div>
+                <div className="text-box" data-aos='fade-up-right' data-aos-duration='1000'><h2>Emotional</h2>  <p>{getTextForEmotional(presentDayEmotional)}</p></div>
+                <div className="text-box" data-aos='fade-up' data-aos-duration='1000'><h2>Average</h2>  <p>{getTextForAverage(presentDayAverage)}</p></div>
               </div>
 
             </>
